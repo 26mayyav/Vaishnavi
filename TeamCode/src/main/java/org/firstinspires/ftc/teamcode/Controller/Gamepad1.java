721bcf4446e30e2203d1e71c2cdf02e53008c99c
vaish <26mayyav@elmbrookstudents.org>
2021-09-11 11:51:42 -0500

    Created Intake file Added a function Added gamepad class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controller/Gamepad1.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Controller;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+
+public class Gamepad1 {
+
+    public Gamepad1(Gamepad gamepad){
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/FirsTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/FirsTeleop.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@TeleOp(name = "FirstTeleop", group = "00-Teleop")
 public class FirsTeleop  extends LinearOpMode {
 
     DcMotor front_left_wheel;
@@ -17,6 +18,15 @@ public class FirsTeleop  extends LinearOpMode {
         front_right_wheel= (DcMotor) hardwareMap.get("front_right_wheel");
         back_left_wheel= (DcMotor) hardwareMap.get("back_left_wheel");
         back_right_wheel= (DcMotor) hardwareMap.get("back_right_wheel");
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            front_left_wheel.setPower(gamepad1.left_stick_y);
+            front_right_wheel.setPower(gamepad1.left_stick_y);
+            back_left_wheel.setPower(gamepad1.left_stick_y);
+            back_right_wheel.setPower(gamepad1.left_stick_y);
+        }
 
     }
 }
