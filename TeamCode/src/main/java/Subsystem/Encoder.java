2770e6e801df17f37b8cd75ae7b4beb0dfe5b511
vaish <26mayyav@elmbrookstudents.org>
2021-09-30 19:50:25 -0500

    Created Intake file Added a function Added gamepad class Added direction and power for wheels Created SSDrive class Added function 2 to encoder class

--- TeamCode/src/main/java/Subsystem/Encoder.java
+++ TeamCode/src/main/java/Subsystem/Encoder.java
@@ -8,7 +8,7 @@ public class Encoder{
     //Object declaration
     DcMotor encoderMotor;
 
-    //variabledeclaration
+    //variableDeclaration
     // datatype variable_name = initial_value
     static  final int MOTOR_TICK_COUNT = 1120;
     double diameter=28;
@@ -17,20 +17,20 @@ public class Encoder{
     //constructor
     public void Encoder(HardwareMap hardwareMap){
 
-        encoderMotor =hardwareMap.dcMotor.get("encoder_motor");
+        encoderMotor = hardwareMap.dcMotor.get("encoder_motor");
 
     }//end of constructor
 
     //functions of this class
-    //identifier return_type function_name(input variabl1,inputvariable 2){}
+    //identifier return_type function_name(input variable1,inputVariable 2){}
 
-    public void resetMotor(){
+    public void (){
 
         encoderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
     }//end of function resetMotor
 
-    public void calculateTargetPosition(double targetDistance){
+    public int calculateTargetPosition(double targetDistance){
 
         double circumference=Math.PI*diameter;//distance traveled in one rotation
         double rotationsMoved=0;
@@ -40,11 +40,46 @@ public class Encoder{
         rotationsMoved = targetDistance/circumference;//Calculated rotations needed for moving target distance
         TICK_COUNT = (int)(rotationsMoved*MOTOR_TICK_COUNT);
 
-
+        return TICK_COUNT;
 
 
     }//end of function calculateTargetPosition
 
+    //function to move motor to targetPosition
+
+    //Object declaration
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    static  final int setPower = 0.5
+
+    // datatype variable_name = initial_value
+    public void resetMotor(){
+
+        encoderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+    leftMotor.setTargetPosition(encoderDrivingTarget);
+
+    leftMotor.setPower(0.5);
+
+    leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    leftMotor.isBusy();
+
+            while(leftMotor.isBusy();
+            rightMotor.isBusy();
+
+        final int telemetry = 0;
+        telemetry.addData('Path','Still driving');
+        telemetry.update();
+
+
+    leftMotor.setPower(0);
+    rightMotor.setPower(0);
+
+
 
 
 }//end of class
+}
